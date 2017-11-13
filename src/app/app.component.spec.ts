095427882bf8e084ca885607323a30d6d8f09f66
Vincent.Morainville <vincent.morainville@ext.betc.com>
2017-11-13 12:34:33 +0100

    Update tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -33,6 +33,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('span.title').textContent).toContain('Hi Bro!');
+    expect(compiled.querySelector('h2').textContent).toContain('Hi Bro!');
   }));
 });
