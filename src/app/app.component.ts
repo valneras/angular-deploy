ceb337de8f11487dff3f8a636ec219b81c0288d1
Vincent.Morainville <vincent.morainville@ext.betc.com>
2017-12-06 18:40:59 +0100

    Redeploy test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, HostListener, ViewChild} from '@angular/core';
+import {Component, HostListener, ViewChild, Renderer2} from '@angular/core';
 import {Router, RouterLink} from '@angular/router';
 
 @Component({
@@ -8,7 +8,8 @@ import {Router, RouterLink} from '@angular/router';
 })
 export class AppComponent {
   @ViewChild('content') content;
-  setColor = false;
+  @ViewChild('main') main;
+  toolbarScroll = false;
   year = new Date().getFullYear();
   title = 'Angular demo App by VinZ!!';
   isDarkTheme = true;
@@ -22,12 +23,36 @@ export class AppComponent {
     ...this.navigation,
     { link: 'settings', label: 'Settings' }
   ];
-  constructor(private router: Router){
+  constructor(private router: Router, private renderer : Renderer2){
 
   }
 
   onScrollEvent($event) {
-    console.log($event);
-    this.setColor = this.content.nativeElement.getBoundingClientRect().top < 14;
+    // console.log("top:"+this.content.nativeElement.getBoundingClientRect().top+" / bottom:"+
+    // this.content.nativeElement.getBoundingClientRect().bottom);
+    // console.log(this.content);
+    let scrollTopPosition=$event.target.scrollTop;
+    let scrollHeight=$event.target.scrollHeight;
+    let windowBottomPosition = (scrollTopPosition + scrollHeight);
+    Object.keys(this.main.nativeElement.children).forEach(item => {
+      let currentSection=this.main.nativeElement.children[item];
+      let sectionTopPosition = (currentSection.offsetTop);
+      let sectionHeight = (currentSection.getBoundingClientRect().height);
+      let sectionBottomPosition = (sectionTopPosition+sectionHeight);
+      console.log('itemid:'+currentSection.id+': sectionBottomPosition:'+sectionTopPosition+'sectionHeight'+sectionHeight+' >= scrollTop:'+scrollTopPosition);
+      console.log('itemid:'+currentSection.id+': sectionTopPosition:'+sectionTopPosition+' <= windowBottomPosition:'+windowBottomPosition);
+      // console.log('itemid:'+currentSection.id+' / itemtop:'+sectionTopPosition+' / itemh:'+currentSection.getBoundingClientRect().height+' / itembottom:'+currentSection.getBoundingClientRect().bottom)
+      // if(item.getBoundingClientRect().top)
+      // console.log(this.main.nativeElement.children[item].getBoundingClientRect().top);
+      if ((sectionBottomPosition >= scrollTopPosition) && (sectionTopPosition <= windowBottomPosition)) {
+        this.renderer.addClass(currentSection, 'slidein');
+      } else {
+        this.renderer.removeClass(currentSection, 'slidein');
+        // currentSection.setElementClass(item, "slidein", false);
+      }
+    });
+    this.toolbarScroll = this.content.nativeElement.getBoundingClientRect().top < 18;
+
+
   }
 }
